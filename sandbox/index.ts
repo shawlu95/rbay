fae31950722d9baae4e274e4a1b46fd90c342ffc
Shaw Lu <shawlu95@gmail.com>
2023-01-18 23:37:23 -0800

    hset null, undefined, and nested object

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -5,7 +5,10 @@ const run = async () => {
 	await client.hSet('car', {
 		brand: 'vw',
 		model: 'tiguan',
-		year: 2019
+		year: 2019,
+		owner: undefined || '',
+		accident: null || '',
+		engine: { cylinders: 8 }
 	});
 
 	const car = await client.hGetAll('car');
