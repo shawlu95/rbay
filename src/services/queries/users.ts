ac33118e338f82f4e9d8f1905b1b77542ef0741b
Shaw Lu <shawlu95@gmail.com>
2023-01-19 00:39:31 -0800

    fetch and deserialize user

--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -5,16 +5,29 @@ import { genId } from '$services/utils';
 
 export const getUserByUsername = async (username: string) => {};
 
-export const getUserById = async (id: string) => {};
+export const getUserById = async (id: string) => {
+	const user = await client.hGetAll(userKey(id));
+	console.log('retrieve user from cache', id, user);
+	return deserialize(id, user);
+};
 
 export const createUser = async (attrs: CreateUserAttrs) => {
 	const id = genId();
 	await client.hSet(userKey(id), serialize(attrs));
+	console.log('cache user', id, attrs);
 	return id;
 };
 
 const serialize = (user: CreateUserAttrs) => {
 	return {
+		username: user.username,
+		password: user.password
+	};
+};
+
+const deserialize = (id: string, user: { [key: string]: string }) => {
+	return {
+		id,
 		username: user.username,
 		password: user.password
 	};
