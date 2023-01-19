638b06c163173ab7550233b79849a2f88312f43a
Shaw Lu <shawlu95@gmail.com>
2023-01-19 00:47:54 -0800

    fetch session from cache

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,3 +1,5 @@
 export const pageCacheKey = (id: string) => `pc#${id}`;
 
 export const userKey = (id: string) => `u#${id}`;
+
+export const sessionKey = (id: string) => `s#${id}`;
--- src/services/queries/sessions.ts
+++ src/services/queries/sessions.ts
@@ -1,5 +1,24 @@
+import { client } from '$services/redis';
+import { sessionKey } from '$services/keys';
 import type { Session } from '$services/types';
 
-export const getSession = async (id: string) => {};
+export const getSession = async (id: string) => {
+	const session = await client.hGetAll(sessionKey(id));
+	if (Object.keys(session).length === 0) {
+		console.log('session not found for user', id);
+		return null;
+	}
+	console.log('retrieve session from cache', id, session);
+	return deserialize(id, session);
+};
 
 export const saveSession = async (session: Session) => {};
+
+
+const deserialize = (id: string, session: {[key: string]: string}) {
+  return {
+    id,
+    userId: session.userId,
+    username: session.username
+  }
+}
