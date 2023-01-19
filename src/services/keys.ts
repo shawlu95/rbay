31ea846a0ecdcd37fb7e4c064e789ff0cbee96ad
Shaw Lu <shawlu95@gmail.com>
2023-01-19 01:02:21 -0800

    cache and serialize item

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -3,3 +3,5 @@ export const pageCacheKey = (id: string) => `pc#${id}`;
 export const userKey = (id: string) => `u#${id}`;
 
 export const sessionKey = (id: string) => `s#${id}`;
+
+export const itemKey = (id: string) => `i#${id}`;
