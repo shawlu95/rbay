80990874bcd54383dabc983e24fa833bde0c063c
Shaw Lu <shawlu95@gmail.com>
2023-01-22 08:05:16 -0800

    watch item key before placing bid

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -5,29 +5,33 @@ import { DateTime } from 'luxon';
 import { getItem } from './items';
 
 export const createBid = async (attrs: CreateBidAttrs) => {
-	const item = await getItem(attrs.itemId);
+	return client.executeIsolated(async (isolatedClient) => {
+		await isolatedClient.watch(itemKey(attrs.itemId));
+		const item = await getItem(attrs.itemId);
 
-	if (!item) {
-		throw new Error('Item does not exist');
-	}
+		if (!item) {
+			throw new Error('Item does not exist');
+		}
 
-	if (item.price >= attrs.amount) {
-		throw new Error('Bid too low');
-	}
+		if (item.price >= attrs.amount) {
+			throw new Error('Bid too low');
+		}
 
-	if (item.endingAt.diff(DateTime.now()).toMillis() < 0) {
-		throw new Error('Item closed to bidding');
-	}
+		if (item.endingAt.diff(DateTime.now()).toMillis() < 0) {
+			throw new Error('Item closed to bidding');
+		}
 
-	const serialized = serializeBid(attrs.amount, attrs.createdAt.toMillis());
-	Promise.all([
-		client.rPush(itemBidKey(attrs.itemId), serialized),
-		client.hSet(itemKey(item.id), {
-			bids: item.bids + 1,
-			price: attrs.amount,
-			highestBidUserId: attrs.userId
-		})
-	]);
+		const serialized = serializeBid(attrs.amount, attrs.createdAt.toMillis());
+		return isolatedClient
+			.multi()
+			.rPush(itemBidKey(attrs.itemId), serialized)
+			.hSet(itemKey(item.id), {
+				bids: item.bids + 1,
+				price: attrs.amount,
+				highestBidUserId: attrs.userId
+			})
+			.exec();
+	});
 };
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
