8b41e241b2b89606c15b98d64fc7f52b2aa987f9
Shaw Lu <shawlu95@gmail.com>
2023-01-21 22:46:22 -0800

    get liked items for user

--- src/services/queries/likes.ts
+++ src/services/queries/likes.ts
@@ -1,11 +1,17 @@
 import { client } from '$services/redis';
 import { userLikesKey, itemKey } from '$services/keys';
+import { getItems } from './items';
 
 export const userLikesItem = async (itemId: string, userId: string) => {
 	return client.sIsMember(userLikesKey(userId), itemId);
 };
 
-export const likedItems = async (userId: string) => {};
+export const likedItems = async (userId: string) => {
+	// fetch all item ids from this user's like set
+	const ids = await client.sMembers(userLikesKey(userId));
+	// fetch all items with those ids in pipeline
+	return await getItems(ids);
+};
 
 export const likeItem = async (itemId: string, userId: string) => {
 	const inserted = await client.sAdd(userLikesKey(userId), itemId);
