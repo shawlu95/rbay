16677c458b33af0f8cea6d04f2f38a1d532abacb
Shaw Lu <shawlu95@gmail.com>
2023-01-21 23:47:08 -0800

    increment item view count in sorted set

--- src/services/queries/views.ts
+++ src/services/queries/views.ts
@@ -1,1 +1,9 @@
-export const incrementView = async (itemId: string, userId: string) => {};
+import { client } from '$services/redis';
+import { itemKey, itemByViewKey } from '$services/keys';
+
+export const incrementView = async (itemId: string, userId: string) => {
+	await Promise.all([
+		await client.hIncrBy(itemKey(itemId), 'views', 1),
+		await client.zIncrBy(itemByViewKey(), 1, itemId)
+	]);
+};
