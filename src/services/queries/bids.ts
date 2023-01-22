485d3ed28324a389f0602f98dc71738d1b897bf3
Shaw Lu <shawlu95@gmail.com>
2023-01-22 03:10:47 -0800

    serialize and deserialize bid

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -5,3 +5,15 @@ export const createBid = async (attrs: CreateBidAttrs) => {};
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
 	return [];
 };
+
+const serializeBid = (amount: number, createdAt: number) => {
+	return `${amount}:${createdAt}`;
+};
+
+const deserializeBid = (stored: string) => {
+	const [amount, createdAt] = stored.split(':');
+	return {
+		amount: parseFloat(amount),
+		createdAt: DateTime.fromMillis(parseInt(createdAt))
+	};
+};
