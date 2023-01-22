be4bce67bd97077f1092bb390f09ee8e06d53ec0
Shaw Lu <shawlu95@gmail.com>
2023-01-22 00:04:50 -0800

    retrieve soonest ending items from sorted set

--- src/services/queries/items/by-ending-time.ts
+++ src/services/queries/items/by-ending-time.ts
@@ -1,5 +1,14 @@
-export const itemsByEndingTime = async (
-	order: 'DESC' | 'ASC' = 'DESC',
-	offset = 0,
-	count = 10
-) => {};
+import { client } from '$services/redis';
+import { itemByEndingAtKey } from '$services/keys';
+import { getItems } from './items';
+
+export const itemsByEndingTime = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
+	const ids = await client.zRange(itemByEndingAtKey(), Date.now(), '+inf', {
+		BY: 'SCORE',
+		LIMIT: {
+			offset,
+			count
+		}
+	});
+	return await getItems(ids);
+};
