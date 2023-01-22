816af4e67f332da99cb9db0578509fa7bcb8468b
Shaw Lu <shawlu95@gmail.com>
2023-01-22 08:20:25 -0800

    retrieve items ranked by price

--- src/services/queries/items/by-price.ts
+++ src/services/queries/items/by-price.ts
@@ -1,1 +1,31 @@
-export const itemsByPrice = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {};
+import { client } from '$services/redis';
+import { itemKey, itemByPriceKey } from '$services/keys';
+import { deserialize } from './deserialize';
+
+export const itemsByPrice = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
+	let results: any = await client.sort(itemByPriceKey(), {
+		GET: [
+			'#',
+			`${itemKey('*')}->name`,
+			`${itemKey('*')}->views`,
+			`${itemKey('*')}->endingAt`,
+			`${itemKey('*')}->imageUrl`,
+			`${itemKey('*')}->price`
+		],
+		BY: 'nosort', // use implicit order in sorted set
+		DIRECTION: order,
+		LIMIT: {
+			offset,
+			count
+		}
+	});
+
+	const items = [];
+	while (results.length) {
+		const [id, name, views, endingAt, imageUrl, price, ...rest] = results;
+		const item = deserialize(id, { name, views, endingAt, imageUrl, price });
+		items.push(item);
+		results = rest;
+	}
+	return items;
+};
