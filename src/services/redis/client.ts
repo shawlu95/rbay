eca85555dec6f8328a7a2a6444cbed3c11c28808
Shaw Lu <shawlu95@gmail.com>
2023-01-22 09:00:17 -0800

    define a basic lua script

--- src/services/redis/client.ts
+++ src/services/redis/client.ts
@@ -1,11 +1,32 @@
-import { createClient } from 'redis';
+import { createClient, defineScript } from 'redis';
 
 const client = createClient({
 	socket: {
 		host: process.env.REDIS_HOST,
 		port: parseInt(process.env.REDIS_PORT)
 	},
-	password: process.env.REDIS_PW
+	password: process.env.REDIS_PW,
+	scripts: {
+		addOneAndStore: defineScript({
+			NUMBER_OF_KEYS: 1,
+			SCRIPT: `
+				return redis.call('SET', KEYS[1], 1 + tonumber(ARGV[1]))
+			`,
+			transformArguments(key: string, value: number) {
+				// all params to lua must be strings
+				return [key, value.toString()];
+			},
+			transformReply(reply: any) {
+				return reply;
+			}
+		})
+	}
+});
+
+client.on('connect', async () => {
+	await client.addOneAndStore('books:count', 15);
+	const result = await client.get('books:count');
+	console.log('lua test', result);
 });
 
 client.on('error', (err) => console.error(err));
