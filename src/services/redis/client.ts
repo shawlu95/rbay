76bf9d0c41ee00d9c5d313daa32c255ce6ab01b3
Shaw Lu <shawlu95@gmail.com>
2023-01-22 09:19:14 -0800

    use lua script to increment view

--- src/services/redis/client.ts
+++ src/services/redis/client.ts
@@ -1,3 +1,4 @@
+import { itemByViewKey, itemKey, itemViewsKey } from '$services/keys';
 import { createClient, defineScript } from 'redis';
 
 const client = createClient({
@@ -19,6 +20,25 @@ const client = createClient({
 			transformReply(reply: any) {
 				return reply;
 			}
+		}),
+		incrementView: defineScript({
+			NUMBER_OF_KEYS: 3,
+			SCRIPT: `
+				local itemViewsKey = KEYS[1]
+				local itemKey = KEYS[2]
+				local itemByViewKey = KEYS[3]
+				local itemId = ARGV[1]
+				local userId = ARGV[2]
+				local inserted = redis.call("PFADD", itemViewsKey, userId)
+				if inserted == 1 then
+					redis.call('HINCRBY', itemKey, 'views', 1)
+					redis.call('ZINCRBY', itemByViewKey, 1, itemId)
+				end
+			`,
+			transformArguments(itemId: string, userId: string) {
+				return [itemViewsKey(itemId), itemKey(itemId), itemByViewKey(), itemId, userId];
+			},
+			transformReply(reply) {}
 		})
 	}
 });
