b37ba2cb623df02ef85b4acf4bf1a7d42ce88025
Shaw Lu <shawlu95@gmail.com>
2023-01-21 23:49:07 -0800

    clean up

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,16 +1,13 @@
 export const pageCacheKey = (id: string) => `pc#${id}`;
 
 export const userKey = (id: string) => `u#${id}`;
-
 export const sessionKey = (id: string) => `s#${id}`;
-
-// h hash set to store all usernames
-export const usernamesUniqueKey = () => 'usernames:unique';
-
-export const userLikesKey = (id: string) => `u:l#${id}`;
-
-export const usernamesKey = () => 'usernames';
+export const usernamesUniqueKey = () => 'usernames:unique'; // hash set
+export const usernamesKey = () => 'usernames'; // sorted set
 
 // items related keys
 export const itemKey = (id: string) => `i#${id}`;
-export const itemByViewKey = () => 'item:views';
+export const itemByViewKey = () => 'item:views'; // sorted set
+
+// relation
+export const userLikesKey = (id: string) => `u:l#${id}`;
