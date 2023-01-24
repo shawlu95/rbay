e7917f619f1856f85b033e4aed0354d37a3797c6
Shaw Lu <shawlu95@gmail.com>
2023-01-23 23:33:21 -0800

    create index on server startup

--- src/services/redis/client.ts
+++ src/services/redis/client.ts
@@ -1,5 +1,6 @@
 import { itemByViewKey, itemKey, itemViewsKey } from '$services/keys';
 import { createClient, defineScript } from 'redis';
+import { createIndexes } from './create-indexes';
 
 const client = createClient({
 	socket: {
@@ -60,4 +61,12 @@ const client = createClient({
 client.on('error', (err) => console.error(err));
 client.connect();
 
+client.on('connect', async () => {
+	try {
+		console.log('createIndexes');
+		await createIndexes();
+	} catch (err) {
+		console.error(err);
+	}
+});
 export { client };
--- src/services/redis/create-indexes.ts
+++ src/services/redis/create-indexes.ts
@@ -1,8 +1,17 @@
-import { client } from '$services/redis';
+import { client } from './client';
 import { itemIndexKey, itemKey } from '$services/keys';
 import { SchemaFieldTypes } from 'redis';
 
 export const createIndexes = async () => {
+	console.log(typeof client);
+	const indices = await client.ft._list();
+	const exists = indices.find((idx) => idx === itemIndexKey());
+	if (exists) {
+		console.log('index exists. Skip', itemIndexKey());
+		return;
+	}
+
+	console.log('create index', itemIndexKey());
 	await client.ft.create(
 		itemIndexKey(),
 		{
