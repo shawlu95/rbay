e3b7ab21e47a0135e5b2c9c0bd0e8da1252c5d06
Shaw Lu <shawlu95@gmail.com>
2023-01-23 23:41:32 -0800

    parse user query and issue to redisearch

--- src/services/queries/items/search.ts
+++ src/services/queries/items/search.ts
@@ -1,1 +1,26 @@
-export const searchItems = async (term: string, size: number = 5) => {};
+import { client } from '$services/redis';
+import { deserialize } from './deserialize';
+import { itemIndexKey } from '$services/keys';
+
+export const searchItems = async (term: string, size: number = 5) => {
+	const cleaned = term
+		.replaceAll(/[^a-zA-Z0-9 ]/g, '')
+		.trim()
+		.split(' ')
+		.map((w) => (w ? `%${w}%` : ''))
+		.join(' ');
+	if (cleaned === '') {
+		return [];
+	}
+
+	console.log('redisearch query:', cleaned);
+	// automatically search all fields marked as text
+	const { total, documents } = await client.ft.search(itemIndexKey(), cleaned, {
+		LIMIT: {
+			from: 0,
+			size
+		}
+	});
+
+	return documents.map(({ id, value }) => deserialize(id, value as any));
+};
