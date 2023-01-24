f63134fefbd0f13b566d3966ae63a452a8fe0100
Shaw Lu <shawlu95@gmail.com>
2023-01-24 00:21:24 -0800

    executing sort query from dashboard

--- src/services/queries/items/by-user.ts
+++ src/services/queries/items/by-user.ts
@@ -1,3 +1,7 @@
+import { client } from '$services/redis';
+import { itemIndexKey, itemKey } from '$services/keys';
+import { deserialize } from './deserialize';
+
 interface QueryOpts {
 	page: number;
 	perPage: number;
@@ -5,4 +9,25 @@ interface QueryOpts {
 	direction: string;
 }
 
-export const itemsByUser = async (userId: string, opts: QueryOpts) => {};
+export const itemsByUser = async (userId: string, opts: QueryOpts) => {
+	const query = `@ownerId:{${userId}}`;
+	const sortCriteria = opts.sortBy &&
+		opts.direction && {
+			BY: opts.sortBy,
+			DIRECTION: opts.direction
+		};
+
+	const { total, documents } = await client.ft.search(itemIndexKey(), query, {
+		ON: 'HASH',
+		SORTBY: sortCriteria,
+		LIMIT: {
+			from: opts.page * opts.perPage,
+			size: opts.perPage
+		}
+	} as any);
+
+	return {
+		totalPages: Math.ceil(total / opts.perPage),
+		items: documents.map(({ id, value }) => deserialize(id.replace(itemKey(''), ''), value as any))
+	};
+};
